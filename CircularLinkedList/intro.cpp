0bffefb6e3d9bf6eebb7dac62fef223439cdb453
Adnan3974 <i243129@isb.nu.edu.pk>
2025-10-15 11:03:04 +0500

    Circular list

---
+++ CircularLinkedList/intro.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+/*
+Circular singly linked list
+*/
+struct Node{
+    int data;
+    Node* next;
+    Node(){
+        next = NULL;
+    }
+};
+class cList{
+    Node* tail;
+    public: 
+    cList(){
+        tail = NULL;
+    }
+    void insertAtStart(int val){
+        Node* newNode = new Node();
+        newNode->data = val;
+        if(tail == NULL){
+            tail = newNode;
+            tail->next = newNode;
+        }
+        else{
+            newNode->next = tail->next;
+            tail->next = newNode;
+        }
+    }
+    void deleteAtStart(){
+        Node* temp = tail->next;
+        tail->next = temp->next;
+        delete temp; temp = nullptr;
+    }
+    void display(){
+        if (tail == NULL) return;
+        Node* temp = tail->next;
+        do {
+            cout<<temp->data<<" ";
+            temp = temp->next;
+        } while(temp != tail->next);
+        cout<<endl;
+        delete temp; temp = nullptr;
+    }
+};
+int main(){
+    cList list;
+    for(int i=34; i<54; i++){
+        list.insertAtStart(i);
+    }
+    list.display();
+    return 0;
+}
